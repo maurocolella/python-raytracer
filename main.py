edf5197df12edbb11aee41357869b6c91708d64b
Mauro <mauro.ludovico.colella@gmail.com>
2020-05-01 13:59:27 +0800

    Add vector class

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from PIL import Image
 import numpy as np
+from threed import vec3
 
 w, h = 512, 512
 data = np.zeros((h, w, 3), dtype=np.uint8)
 for j in range(0,h) :
     for i in range(0,w) :
-        r = i / w
-        g = j / h
-        b = 0.2
-        data[i,j] = [int(255 * r), int(255 * g), int(255 * b)]
-# data[0:512, 0:512] = [255, 0, 0] # red patch in upper left
+        rgb = vec3.Vec(i / w, j / h, 0.2)
+        data[i,j] = rgb.asColor()
 img = Image.fromarray(data, 'RGB')
 img.save('render.png')
 img.show()
